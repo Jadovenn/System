3e9fa16e703b19c0240ea923dc0c9aaf843c04a1
Jean-Adrien Domage <jadomage-prestataire@vente-privee.com>
2019-03-27 21:24:29 -0700

    add basic mem function from libc

---
+++ arch/x86/lib/libc/include/string.h
@@ -0,0 +1,23 @@
+/**
+ * Netero - C library Strings
+ */
+
+/**
+ * @details copy n bytes from src to dest.
+ * @param dest {void*} - pointer to destination memory
+ * @param src  {const void*} - pointer to source memory
+ * @param size {unsigned long} - number of byte to copy
+ * @return {void*} - original value of dest
+ */
+void	*memcpy(void *dest, const void *src, unsigned long size);
+
+/**
+ * @details set n bytes with values byte to dest
+ * @warning the memory area must be at least size long
+ * @param dest {void*} - pointer to destination memory
+ * @param byte {int} - value of the byte, (converter in unsigned char)
+ * @param size {unsigned long} - number of byte to set
+ */
+void	*memset(void *dest, int byte, unsigned long size);
+
+
