7baaf54e5b1cda7f6d601171a07b7750c120fd0b
Jean-Adrien Domage <jadomage-prestataire@vente-privee.com>
2019-06-04 13:48:45 -0700

    add boolean type

--- include/kernel/types.h
+++ include/kernel/types.h
@@ -5,6 +5,9 @@
 #ifndef TYPES_H_
 # define TYPES_H_
 
+#define true	0
+#define false	1
+
 typedef signed char 		int8_t;
 typedef unsigned char		uint8_t;
 typedef signed short		int16_t;
@@ -13,6 +16,7 @@ typedef signed int		int32_t;
 typedef	unsigned int		uint32_t;
 typedef signed long long	int64_t;
 typedef unsigned long long	uint64_t;
+typedef uint8_t			bool;
 
 #endif // TYPES_H_
 
