94c7ea8866d121845e6e7436472f438110f20a4d
Jean-Adrien Domage <jadomage-prestataire@vente-privee.com>
2019-06-04 13:41:14 -0700

    rename main header system

---
+++ include/system.h
@@ -0,0 +1,19 @@
+/**
+ *	System Kernel main include
+ */
+
+#ifndef SYSTEM_H_
+# define SYSTEM_H_
+
+#define VERSION "0.0.1"
+
+#define NULL ((void*)0)
+
+#define SUCCESS	0
+#define ERROR	1
+
+#define TRUE	1
+#define FALSE	0
+
+#endif // SYSTEM_H_
+
