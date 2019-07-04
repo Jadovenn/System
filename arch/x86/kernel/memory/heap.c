ad562e796f203bd73494977a6b061bc57cbd2b04
Jean-Adrien Domage <jadomage-prestataire@vente-privee.com>
2019-07-04 00:10:26 -0700

    add non freeable heap management, pre-paging memory

--- arch/x86/kernel/memory/heap.c
+++ arch/x86/kernel/memory/heap.c
@@ -3,42 +3,38 @@
  * System sources under license MIT
  */
 
+#include <system.h>
 #include <kernel/types.h>
 
 extern uint32_t	end; // defined in the linker script
 uint32_t	placement_address = (uint32_t)&end;
 
-uint32_t	kmalloc(uint32_t size) {
+static uint32_t	__kmalloc_internal(uint32_t size, bool align, uint32_t *physical_addr) {
+	if (align == TRUE && (placement_address & 0xfffff000)) {
+		placement_address &= 0xfffff000;
+		placement_address += 0x1000;
+	}
+	if (physical_addr) {
+		*physical_addr = placement_address;
+	}
 	uint32_t tmp = placement_address;
 	placement_address += size;
 	return tmp;
+}
+
+uint32_t	kmalloc(uint32_t size) {
+	return __kmalloc_internal(size, FALSE, NULL);
 }
 
 uint32_t	kmalloc_aligned(uint32_t size) {
-	placement_address &= 0xFFFFF000;
-	placement_address += 0x1000;
-	uint32_t tmp = placement_address;
-	placement_address += size;
-	return tmp;
+	return __kmalloc_internal(size, TRUE, NULL);
 }
 
 uint32_t	kmalloc_physical(uint32_t size, uint32_t *phys) {
-	if (phys) {
-		*phys = placement_address;
-	}
-	uint32_t tmp = placement_address;
-	placement_address += size;
-	return tmp;
+	return __kmalloc_internal(size, FALSE, phys);
 }
 
 uint32_t	kmalloc_physical_align(uint32_t size, uint32_t *phys) {
-	placement_address &= 0xFFFFF000;
-	placement_address += 0x1000;
-	if (phys) {
-		*phys = placement_address;
-	}
-	uint32_t tmp = placement_address;
-	placement_address += size;
-	return tmp;
+	return __kmalloc_internal(size, TRUE, phys);
 }
 
