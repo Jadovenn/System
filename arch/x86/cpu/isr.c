dffe0821320a6bdb1565630fe6271914a5c74274
Jean-Adrien Domage <jadomage-prestataire@vente-privee.com>
2019-07-04 22:38:34 -0700

    fix missing interrupt reset not send to PIC master while comming from PIC slave

--- arch/x86/cpu/isr.c
+++ arch/x86/cpu/isr.c
@@ -19,7 +19,7 @@ int	register_interrupt_handler(uint8_t index, isr_t handler) {
 }
 
 void	isr_handler(registers_t regs) {
-	printk("Recived Interruption\n");
+	printk("Fault: %d\n", regs.interr_nb);
 	if (interrupt_handlers_map[regs.interr_nb] != 0) {
 		isr_t handler = interrupt_handlers_map[regs.interr_nb];
 		handler(regs);
@@ -30,9 +30,7 @@ void	irq_handler(registers_t regs) {
 	if (regs.interr_nb >= 40) {
 		port_write_byte(0xa0, 0x20);
 	}
-	else {
-		port_write_byte(0x20, 0x20);
-	}
+	port_write_byte(0x20, 0x20);
 	if (interrupt_handlers_map[regs.interr_nb] != 0) {
 		isr_t handler = interrupt_handlers_map[regs.interr_nb];
 		handler(regs);
