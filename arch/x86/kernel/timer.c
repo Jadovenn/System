830587ee2303209f3fd2c408cab00086da53b231
Jean-Adrien Domage <jadomage-prestataire@vente-privee.com>
2019-07-05 13:49:08 -0700

    fix tick counter not intialize to 0, add comment

--- arch/x86/kernel/timer.c
+++ arch/x86/kernel/timer.c
@@ -11,7 +11,7 @@
 #include <kernel/ports.h>
 #include <drivers/monitor.h>
 
-uint32_t	tick;
+uint32_t	tick = 0;
 
 static void __timer_callback(registers_t regs) {
 	tick++;
@@ -21,12 +21,21 @@ static void __timer_callback(registers_t regs) {
 	}
 }
 
+/**
+ * @brief register PIT IRQ
+ * @details config register value send to port 0x43:
+ * bit [0] = 0: binary counter 16bits
+ * bit [1-3] = x11: square wave generator
+ * bit [4-5] = 11: counter bits 0-7 first, then 8-15
+ * bit [5-7] = 0: counter 0 select
+ * @param frequency - freq for IRQ to be trigger by the PIT
+ */
 void	init_timer(uint32_t frequency) {
 	int ret = register_interrupt_handler(IRQ_0, &__timer_callback);
 	if (ret == ERROR)
 		printk("register error\n");
 	uint32_t divisor = 1193180 / frequency;
-	port_write_byte(0x43, 0x36);
+	port_write_byte(0x43, 0b00110110);
 	uint8_t	low = (uint8_t)(divisor & 0xff);
 	uint8_t hight = (uint8_t)((divisor >> 8) & 0xff);
 	port_write_byte(0x40, low);
