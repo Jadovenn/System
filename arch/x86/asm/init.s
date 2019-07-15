b2de0bebd8a0d4ba8643eb8482ca8de2d3a488e5
Jean-Adrien Domage <jadomage-prestataire@vente-privee.com>
2019-07-14 20:14:18 -0700

    remove paging related in _init proc

--- arch/x86/asm/init.s
+++ arch/x86/asm/init.s
@@ -1,20 +1,13 @@
 [BITS 32]
 [GLOBAL _init]
+[EXTERN entrypgdir]
 
 ;; -----------------------------------------------------
 ;; init.s - kernel early control register initialization
 ;; System sources under license MIT
 ;; -----------------------------------------------------
 
-%define	CR4_PSE	0x00000010
-
 section .text
 _init:
-	push	eax
-	;; set page size up to 4Mib
-	mov	eax, cr4
-	or	eax, CR4_PSE
-	mov	cr4, eax
-	pop	eax
 	ret
 
