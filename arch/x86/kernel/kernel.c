e0a4b24f4f53c645ce054b2f8565da9db20e109f
Jean-Adrien Domage <jadomage-prestataire@vente-privee.com>
2019-07-16 00:25:41 -0700

    add install and config hook for mmu in kmain

--- arch/x86/kernel/kernel.c
+++ arch/x86/kernel/kernel.c
@@ -10,39 +10,44 @@
 #include "init/init.h"
 
 /**
- * @details install global descriptors tables and
- * interrupt descriptor table
+ * @brief install CPU related unit
  */
 void	config_cpu() {
 	install_gdt();
 	install_idt();
+	install_mmu();
 	asm volatile ("sti");
 }
 
 /**
  * multiboot info header parser
  */
-void	init_with_multiboot(multiboot_info *header, uint32_t magic) {
+void	check_multiboot(multiboot_info *header, uint32_t magic) {
 	if (magic != MULTIBOOT_BOOTLOADER_MAGIC) {
-		PANIC("Wrong multiboot magic number, multiboot header is not present");
+		PANIC("Wrong multiboot magic number, \
+				multiboot header is not present");
 	}
 	if (!(header->flags & MULTIBOOT_INFO_MEMORY)) {
-		PANIC("Multiboot memmory flags is not present, manual memory detection is not available");
+		PANIC("Multiboot memmory flags is not present, \
+				manual memory detection is not available");
 	}
 	if (!(header->flags & MULTIBOOT_INFO_MMAP)) {
-		PANIC("Multiboot memory map is not present, manual memory detection is not available");
+		PANIC("Multiboot memory map is not present, \
+				manual memory detection is not available");
 	}
-	printk("Lower Memory: %dKiB\n", header->mem_lower);
-	printk("Upper Memory: %dKiB\n", header->mem_upper);
+	printk("Lower Memory Size: %dKiB\n", header->mem_lower);
+	printk("Upper Memory Size: %dKiB\n", header->mem_upper);
 }
 
 /**
- * @details kernel multiboot entrypoint
+ * @brief kernel entrypoint
+ * @details the order init function are called matter a lot
  */
 void	kmain(multiboot_info *header, uint32_t magic) {
 	config_cpu();
 	monitor_init();
-	init_with_multiboot(header, magic);
+	check_multiboot(header, magic);
+	configure_mmu(header);
 	init_timer(50);
 	while(1);
 }
