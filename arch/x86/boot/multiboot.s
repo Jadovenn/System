e3af7fa028cacdfb2ffe615542493344dab23af5
Jean-Adrien Domage <jadomage-prestataire@vente-privee.com>
2019-07-15 22:00:36 -0700

    in multiboot.s, remove _init refference, comment, typo

--- arch/x86/boot/multiboot.s
+++ arch/x86/boot/multiboot.s
@@ -8,7 +8,6 @@
 [EXTERN end]
 ;; symbole from the kernel
 [EXTERN kmain]
-[EXTERN _init]
 ;; ------------------------------------
 ;; multiboot.s - multiboot kernel entry point
 ;; System sources under license MIT
@@ -50,13 +49,15 @@ stack_bottom:
 resb	0x4000
 stack_top:
 
-
 section .text
 global _start:function (_start.end - _start)
 _start:
+	;; eax and ebx are reserved, do not alter them until they are saved
+	;; Map free space in lower half, this is where grub put the
+	;; multiboot header, so we need it
 	mov	edi, V2P(boot_page_table) + 0x7 * 0x4
 	mov	esi, 0x7000
-.higher_half_maping:
+.free_lower_half_maping:
 	cmp	esi, 0x7FFFF
 	jg	.begin_kernel_maping
 	mov	edx, esi
@@ -64,13 +65,12 @@ _start:
 	mov	[edi], edx
 	add	esi, 0x1000
 	add	edi, 0x4
-	loop	.higher_half_maping
-
+	loop	.free_lower_half_maping
 .begin_kernel_maping:
 	mov	edi, V2P(boot_page_table)
 	mov	esi, 0x00
 	mov	ecx, 0x3FF
-	;; Map the kernel right now
+	;; Map the kernel 
 	;; TODO: messy, improve this
 .kernel_maping:
 	;; while we are below the kernel inc esi
@@ -82,25 +82,17 @@ _start:
 	mov	edx, esi
 	or	edx, 0x003
 	mov	[edi], edx
-
 .inc_esi:
 	add	esi, 0x1000
 	add	edi, 0x4
 	loop	.kernel_maping
-	
 .vga_mem_maping:
 	;; map vga memory to 0xC03FF000, present and writable
 	mov	DWORD [V2P(boot_page_table) + 0x3FF * 0x4], 0x000B8000 | 0x003
-
 	;; Map page table for virtual address 0x00000000
 	mov	DWORD [V2P(boot_page_directory)], V2P(boot_page_table) + 0x003
 	;; Map page table for virtual adress 0xC0000000
 	mov	DWORD [V2P(boot_page_directory) + 768 * 4], V2P(boot_page_table) + 0x003 
-
-	;; set page size up to 4MiB
-	;; mov	ecx, cr4
-	;; or	ecx, CR4_PSE
-	;; mov	cr4, ecx
 	;; Set page directory
 	mov	ecx, V2P(boot_page_directory) 
 	mov	cr3, ecx
@@ -109,24 +101,19 @@ _start:
 	or	ecx, 0x80010000
 	mov	cr0, ecx
 	;; even after paging enabled, PC is still related to physical address
-	;; that why we map this page to both 0x00... and 0xC00..., we must manualy jump to the higher half
+	;; that why we map this page to both 0x00... and 0xC01..., we must manualy jump to the higher half
 	;; and avoid page fault
 	mov	ecx, _start.higher_half_addr
 	jmp	ecx
-
 .higher_half_addr:
 	;; After this label, paging should be enable, and virtual address effective
 	mov	DWORD [boot_page_directory], 0
 	;; reload cr3 to force TLB flush, change take effect
 	invlpg	[0]
-	;;mov	ecx, boot_page_directory
-	;;mov	cr3, ecx
-
 	mov	esp, stack_top
 	add	ebx, 0xC0000000
 	push	eax
 	push	ebx
-	;;call	_init
 	call	kmain
 	cli
 .hang:	hlt
