0995dd704c9fa3dece99024b7c68842f7df3355a
Jean-Adrien Domage <jadomage-prestataire@vente-privee.com>
2019-07-16 17:15:01 -0700

    remap text and rodata section to present, read only in mmu_init.c

--- arch/x86/kernel/init/mmu_init.c
+++ arch/x86/kernel/init/mmu_init.c
@@ -8,9 +8,9 @@
 #include <kernel/memlayout.h>
 #include <cpu/mmu.h>
 
-void	_set_section_text_ro() {
+static void	__set_section_text_ro() {
 	uint32_t text_start_addr = VIRTUAL_ADDR_TO_PHYSICAL(&_kernel_start);
-	uint32_t text_end_addr = VIRTUAL_ADDR_TO_PHYSICAL(&ecode);
+	uint32_t text_end_addr = VIRTUAL_ADDR_TO_PHYSICAL(&_end_code);
 	uint32_t page_offset = text_start_addr / 0x1000;
 	uint32_t pte_addr = mmu.boot_page_table[page_offset] & 0xFFFFF000; 
 	while (pte_addr < text_end_addr) {
@@ -18,8 +18,18 @@ void	_set_section_text_ro() {
 		page_offset += 1;
 		pte_addr = mmu.boot_page_table[page_offset] & 0xFFFFF000; 
 	}
-	__asm__("mov %%cr3, %%eax" : : );
-	__asm__("mov %%eax, %%cr3" : : );
+}
+
+static void	__set_section_rodata_ro() {
+	uint32_t rodata_start_addr = VIRTUAL_ADDR_TO_PHYSICAL(&_rodata);
+	uint32_t rodata_end_addr = VIRTUAL_ADDR_TO_PHYSICAL(&_end_rodata);
+	uint32_t page_offset = rodata_start_addr / 0x1000;
+	uint32_t pte_addr = mmu.boot_page_table[page_offset] & 0xFFFFF000;
+	while (pte_addr < rodata_end_addr) {
+		mmu.boot_page_table[page_offset] = pte_addr | 0x001;
+		page_offset += 1;
+		pte_addr = mmu.boot_page_table[page_offset] & 0xFFFFF000; 
+	}
 }
 
 // TODO: init global mmu struct, retrieve boot page directory and PTE
@@ -29,7 +39,10 @@ void	install_mmu() {
 		return;
 	mmu.boot_page_directory = &boot_page_directory;
 	mmu.boot_page_table = &boot_page_table;
-	_set_section_text_ro();
+	__set_section_text_ro();
+	__set_section_rodata_ro();
+	__asm__("mov %%cr3, %%eax" : : );
+	__asm__("mov %%eax, %%cr3" : : );
 }
 
 // TODO: map free region using the multiboot header
