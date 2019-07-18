3731e5e250b358cee97cfb39c14ad140deb3f69e
Jean-Adrien Domage <jadomage-prestataire@vente-privee.com>
2019-07-18 01:07:19 -0700

    add get_new_heap_page func

---
+++ arch/x86/cpu/mmu/heap.c
@@ -0,0 +1,24 @@
+/**
+ * heap.c - Kernel Heap Management
+ * System sources under license MIT
+ */
+
+#include <kernel/memlayout.h>
+#include <cpu/cr.h>
+#include <cpu/mmu.h>
+
+// TODO: actually we have a dummy physical memory management,
+// and assum that this will never been free because
+// kmalloc will keep the pages for futher allocation
+// It would be better to use the save function for all
+// page allocation
+void	*get_new_heap_page() {
+	const size_t page_size = get_page_size();
+	uint32_t new_page = mmu.heap.placement_address + mmu.heap.page_count * page_size;
+	mmu.heap.page_count += 1;
+	mmu.heap.top = PHYSICAL_PTR_TO_VIRTUAL(new_page);
+	mmu.boot_page_table[new_page / page_size] = new_page | 0x003;
+	flush_tlb();
+	return PHYSICAL_PTR_TO_VIRTUAL(new_page);
+}
+
