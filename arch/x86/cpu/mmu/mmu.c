1a2c65d279b65c46925bf92c5a321d62733862d7
Jean-Adrien Domage <jadomage-prestataire@vente-privee.com>
2019-07-18 01:04:34 -0700

    add kernel_heap data struct in mmu struct

--- arch/x86/cpu/mmu/mmu.c
+++ arch/x86/cpu/mmu/mmu.c
@@ -6,5 +6,19 @@
 #include <system.h>
 #include <cpu/mmu.h>
 
-struct mmu_t	mmu = {NULL, NULL};
+struct mmu_t	mmu = { 
+	NULL,
+	NULL,
+	0,
+	{
+		NULL,
+		NULL,
+		0,
+		0
+	}
+};
+
+size_t	get_page_size() {
+	return mmu.page_size;
+}
 
--- arch/x86/include/cpu/mmu.h
+++ arch/x86/include/cpu/mmu.h
@@ -15,13 +15,31 @@
 #include <kernel/types.h>
 #include <kernel/multiboot.h>
 
+/**
+ * @brief return the physical page size
+ * @details it is accessible via the global object
+ * but this is not guarantee to be the case in the future.
+ * Prefer to use this getter instead.
+ */
+size_t	get_page_size();
+
+void	*get_new_heap_page();
+
+typedef struct 		kernel_heap_t {
+	uint32_t	*base;
+	uint32_t	*top;
+	uint32_t	placement_address;
+	size_t		page_count;
+} 			kheap;
+
 struct mmu_t	{
 	uint32_t	*boot_page_directory;
 	uint32_t	*boot_page_table;
+	size_t		page_size;
+	kheap		heap;
 };
 
 extern struct mmu_t	mmu;
-
 
 #endif // MMU_H_
 
