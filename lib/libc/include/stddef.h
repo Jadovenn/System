1f01d6a8e74093208477e238550f1f70caed296a
Jean-Adrien Domage <jadomage-prestataire@vente-privee.com>
2019-07-23 21:24:13 -0700

    remove dummy types header files, add stdint, stddef, stdbool

---
+++ lib/libc/include/stddef.h
@@ -0,0 +1,16 @@
+/**
+ * stddef.h - Standard definition
+ * System sources under license MIT
+ */
+
+#ifndef STDDEF_H_
+# define STDDEF_H_
+
+#define NULL	((void*)0)
+
+typedef ptrdiff_t	signed int;
+typedef wchar_t		unsigned char;
+typedef size_t		unsigned int;
+
+#endif // STDDEF_H_
+
---
+++ lib/libc/include/stdint.h
@@ -0,0 +1,41 @@
+/**
+ * stdint.h - Standard integers
+ * System sources under license MIT
+ */
+
+#ifndef STDINT_H_
+# define STDINT_H_
+
+typedef signed char 		int8_t;
+typedef signed short		int16_t;
+typedef signed int		int32_t;
+typedef signed long long	int64_t;
+
+typedef unsigned char		uint8_t;
+typedef unsigned short		uint16_t;
+typedef	unsigned int		uint32_t;
+typedef unsigned long long	uint64_t;
+
+// TODO: Not shure about that
+typedef int8_t		int_least8_t;
+typedef int16_t		int_least16_t;
+typedef int32_t		int_least32_t;
+typedef int64_t		int_least64_t;
+
+typedef uint8_t		uint_least8_t;
+typedef uint16_t	uint_least16_t;
+typedef uint32_t	uint_least32_t;
+typedef uint64_t	uint_least64_t;
+
+typedef int_least8_t	int_fast8_t;
+typedef int_least16_t	int_fast16_t;
+typedef int_least32_t	int_fast32_t;
+typedef int_least64_t	int_fast64_t;
+
+typedef uint_least8_t	uint_fast8_t;
+typedef uint_least16_t	uint_fast16_t;
+typedef uint_least32_t	uint_fast32_t;
+typedef uint_least64_t	uint_fast64_t;
+
+#endif // STDINT_H_
+
