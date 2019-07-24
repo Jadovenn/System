eb0afdf29b01f4375e8fd13acf80afe65c6331a4
Jean-Adrien Domage <jadomage-prestataire@vente-privee.com>
2019-07-23 22:15:47 -0700

    move PANIC macro defintion in it own header panic.h

--- include/system.h
+++ include/system.h
@@ -8,22 +8,5 @@
 
 #define VERSION "0.0.2"
 
-#include <kernel/stdio.h>
-
-#define NULL ((void*)0)
-
-#define SUCCESS	0
-#define ERROR	1
-
-#define TRUE	1
-#define FALSE	0
-
-#define PANIC(format, ...)	do {\
-	printk("Sytem panic in %s at line %d:\n", __FILE__, __LINE__);\
-	printk(format __VA_OPT__(,) __VA_ARGS__);\
-	printk("\n!!!!!! KERNEL PANIC !!!!!!\n");\
-	while(1);\
-} while (0)
-
 #endif // SYSTEM_H_
 
