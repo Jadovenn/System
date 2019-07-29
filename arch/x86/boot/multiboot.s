0395f13646d3477cab4104272e1c104a4dd8b584
Jean-Adrien Domage <jadomage-prestataire@vente-privee.com>
2019-07-28 23:58:17 -0700

    add relative path frome root of the repository

--- arch/x86/boot/multiboot.s
+++ arch/x86/boot/multiboot.s
@@ -15,7 +15,7 @@
 ;; System sources under license MIT
 ;; ------------------------------------
 
-%include "./boot/memlayout.s"
+%include "arch/x86/boot/memlayout.s"
 
 ;; Constant
 MBALIGN  equ	1 << 0
