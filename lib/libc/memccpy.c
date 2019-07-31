78aa18a70e8afc4f0446d5524559b1bbbccd9cb4
Jean-Adrien Domage <jadomage-prestataire@vente-privee.com>
2019-07-30 23:13:26 -0700

    fix warnings in memccpy, wronf const qualifier usage

--- lib/libc/memccpy.c
+++ lib/libc/memccpy.c
@@ -9,13 +9,13 @@
 #include <bitset.h>
 
 void	*memccpy(void *restrict target, const void *restrict source, int c, size_t n) {
-	uint32_t *word_ptr_target = NULL;
-	uint32_t *word_ptr_source = NULL;
-	uint8_t  *byte_ptr_target = target;
-	uint8_t	 *byte_ptr_source = source;
-	uint8_t	 character = (uint8_t)c;
-	unsigned idx = 0;
-	void	 *occurence_ptr = NULL;
+	uint32_t	*word_ptr_target = NULL;
+	const uint32_t	*word_ptr_source = NULL;
+	uint8_t		*byte_ptr_target = target;
+	const uint8_t	*byte_ptr_source = source;
+	uint8_t		character = (uint8_t)c;
+	unsigned	idx = 0;
+	void		*occurence_ptr = NULL;
 
 	while ((n - idx) % 4 != 0) { // Read first byte
 		byte_ptr_target[idx] = byte_ptr_source[idx];
@@ -25,8 +25,8 @@ void	*memccpy(void *restrict target, const void *restrict source, int c, size_t 
 		}
 		idx += 1;
 	}
-	word_ptr_target = byte_ptr_target + idx; // use aligned pointer
-	word_ptr_source = byte_ptr_source + idx;
+	word_ptr_target = (uint32_t*)byte_ptr_target + idx; // use aligned pointer
+	word_ptr_source = (uint32_t*)byte_ptr_source + idx;
 	for (; idx < n; idx += 4) { // read word by word
 		uint32_t chunck = word_ptr_source[idx / 4];
 		if (_has_byte(chunck, character)) { // if char is found, go byte by byte and return
