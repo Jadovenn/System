dda0f7da7990fb49c4e08da2fe27c711f8c1bf14
Jean-Adrien Domage <jadomage-prestataire@vente-privee.com>
2019-07-30 23:57:56 -0700

    fix warnings in strstr

--- lib/libc/strstr.c
+++ lib/libc/strstr.c
@@ -9,7 +9,7 @@ char	*strstr(const char *s1, const char *s2) {
 	unsigned idx, jdx;
 	char	*track = NULL;
 	if (!s2) {
-		return s1;
+		return (char*)s1;
 	}
 	if (!s1) {
 		return NULL;
@@ -17,7 +17,7 @@ char	*strstr(const char *s1, const char *s2) {
 	for (idx = 0, jdx = 0; s1[idx]; idx++) {
 		if (s2[idx] == s1[jdx]) {
 			if (idx == 0) {
-				track = s2 + idx;
+				track = (char*)s2 + idx;
 			}
 			jdx += 1;
 		} else {
