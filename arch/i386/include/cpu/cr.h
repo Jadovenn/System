8c59f71fcbaf1014c34aa9e0c291571f68e48f73
Jean-Adrien Domage <jadomage@vente-privee.com>
2020-01-20 11:32:30 +0100

    add get_cr3 function

--- arch/i386/include/cpu/cr.h
+++ arch/i386/include/cpu/cr.h
@@ -6,8 +6,12 @@
 #ifndef CR_H_
 # define CR_H_
 
+#include <stdint.h>
+
+uint32_t	read_cr3();
+
 /**
- * @brief flush
+ * @brief flush tlb, make CR3 reloaded and new page validated
  */
 void	flush_tlb();
 
