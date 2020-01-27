7cbf286fbe4805e699841e6db640950c07deb1d5
Jean-Adrien Domage <jadomage@vente-privee.com>
2020-01-27 15:59:47 +0100

    add physical memory manager bitset region management function

--- arch/i386/physical_memory/physical_memory.c
+++ arch/i386/physical_memory/physical_memory.c
@@ -3,21 +3,56 @@
  * System sources under license MIT
  */
 
+#include <stdlib.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <bitset.h>
 
-extern const char *_end;
+#include "physical_memory.h"
 
-struct physical_memory_group *_kmem;
-
-void	pmm_init1(void *vstart, void *vend) {
-	(void)vstart;
-	(void)vend;
-	_kmem = NULL;
+uint32_t	pmm_set_page(const uint32_t p_addr, bool value) {
+	pmm_region_t *region = NULL;
+	if (p_addr % 0x1000) { // check alignement
+		return EXIT_FAILURE;
+	}
+	{ // find coresponding region
+		pmm_region_t *idx = physical_mmap;
+		while (idx) {
+			if (p_addr - idx->physical_addr < idx->page_nb * 0x1000) {
+				region = idx;
+				break;
+			}
+			idx = idx->next;
+		}
+		if (!region) { // if addr not accessable
+			return EXIT_FAILURE;
+		}
+	}
+	uint32_t offset = ((p_addr - region->physical_addr) / 0x1000) / 32;
+	uint32_t bit = ((p_addr - region->physical_addr) / 0x1000) % 32;
+	bit = 31 - bit;
+	if (value) {
+		_set_bit(region->bitset[offset], bit);
+	}
+	else {
+		_unset_bit(region->bitset[offset], bit);
+	}
+	return EXIT_SUCCESS;
 }
 
-void	physical_free(void) {
+uint32_t	pmm_set_region(const uint32_t p_start_addr, const uint32_t p_end_addr, bool value) {
+	uint32_t offset = p_start_addr;
+	if (p_start_addr > p_end_addr) {
+		return EXIT_FAILURE;
+	}
+	while (offset <= p_end_addr) {
+		pmm_set_page(offset, value);
+		offset += 0x1000;
+	}
+	return EXIT_SUCCESS;
+}
 
+void	p_free(void) {
 }
 
 /**
@@ -25,7 +60,7 @@ void	physical_free(void) {
  * @details sequencial search algorithm using shortcut ptr
  * @return a physical pointer to a 4Kib physical page
  */
-void	*physical_alloc(void) {
+void	*p_alloc(void) {
 	return NULL;
 }
 
--- arch/i386/physical_memory/physical_memory.h
+++ arch/i386/physical_memory/physical_memory.h
@@ -6,22 +6,40 @@
 #ifndef PHYSICAL_MEMORY_H_
 # define PHYSICAL_MEMORY_H_
 
+#include <stdint.h>
+#include <stdbool.h>
+
 // physical memory region
-typedef struct pm_region {
+typedef struct pmm_region {
 	uint32_t	physical_addr;
 	uint32_t	next_free_offset;
 	size_t		page_nb;
 	uint32_t	*bitset;
-	struct 		pm_region *next;
-}		pm_region_t;
+	struct 		pmm_region *next;
+}		pmm_region_t;
 
-extern pm_region_t *physical_mmap;
+extern pmm_region_t *physical_mmap;
 
 void	physical_free(void);
 
 void	*physical_alloc(void);
 
-uint32_t	pmark_page(uint32_t p_addr);
+/**
+ * @brief set value to physical page
+ * @param p_addr - aligned physcial page's addr
+ * @param value - value to set true/false
+ * @return EXIT_SUCCESS/FAILURE
+ */
+uint32_t	pmm_set_page(uint32_t p_addr, bool value);
+
+/**
+ * @brief set value to physical region including p_end_addr
+ * @param p_start_addr - first page addr to map aligned
+ * @param p_end_addr - last including page to map aligned
+ * @param value - value to set true/false
+ * @return EXIT_SUCCESS/FAILURE
+ */
+uint32_t	pmm_set_region(const uint32_t p_start_addr, const uint32_t p_end_addr, bool value);
 
 #endif // PHYSICAL_MEMORY_H_
 
