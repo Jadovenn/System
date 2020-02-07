ecddbb2e1a18a9f3ebceb40d10b5ca28c4674fec
Jean-Adrien Domage <jadomage@vente-privee.com>
2020-02-07 14:29:26 +0100

    add paging_translate function

--- arch/i386/include/arch/paging.h
+++ arch/i386/include/arch/paging.h
@@ -8,10 +8,29 @@
 
 #include <stdbool.h>
 #include <stdint.h>
-#include "cpu/isr.h"
+#include <cpu/isr.h>
 
-uint32_t	paging_map_physical(uint32_t physical_addr, uint32_t virtual_addr, uint32_t flags, bool override);
-void		page_fault_handler(registers_t regs);
+/**
+ * @brief map a physical page to a virtual page
+ * @param physcial_addr - aligned physical page addr
+ * @param virtual_addr - aligned virtual page addr
+ * @param flags - page configuration flags
+ * @param override - if true override existing setting if there is a present valide page
+ */
+uint32_t paging_map_physical(uint32_t physical_addr, uint32_t virtual_addr, uint32_t flags, bool override);
+
+/**
+ * @brief page fault handler
+ */
+void page_fault_handler(registers_t regs);
+
+/**
+ * @brief translate a virtual address to physical addr
+ * @param virtual_addr - virtual address to translate, not
+ * 	necessarly aligned
+ * @return {uint32_t} - corresponding physical address
+ */
+uint32_t paging_translate_virtual(uint32_t virtual_addr);
 
 #endif // PAGING_H_
 
