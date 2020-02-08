3cca56bdf61a515457e42dee92eba621d2d9ceb7
Jean-Adrien Domage <jadomage@vente-privee.com>
2020-02-08 22:39:58 +0100

    add missing global region marker in memlayout.h

--- arch/i386/include/arch/memlayout.h
+++ arch/i386/include/arch/memlayout.h
@@ -16,7 +16,17 @@
 #define VIRTUAL_ADDR_TO_PHYSICAL(x)	(uint32_t)(((uint32_t)x) - VIRTUAL_KERNEL_SPACE_ADDR)
 #define PHYSICAL_ADDR_TO_VIRTUAL(x)	(uint32_t)(((uint32_t)x) + VIRTUAL_KERNEL_SPACE_ADDR)
 
+/**
+ * Globale variable here are in order they shoul be in memory
+ */
+
+/**
+ * Defined in ld script
+ */
+extern uint32_t _kernel_space;
 extern uint32_t	_kernel_start;
+extern uint32_t	boot_page_directory;
+extern uint32_t	boot_page_table;
 extern uint32_t	_code;
 extern uint32_t	_end_code;
 extern uint32_t	_rodata;
@@ -25,8 +35,22 @@ extern uint32_t	_end_data;
 extern uint32_t	_bss;
 extern uint32_t	_end;
 
-extern uint32_t	boot_page_directory;
-extern uint32_t	boot_page_table;
+/**
+ * Managed by physical memory manager
+ */
+extern uint32_t _physical_mmap_start;
+extern uint32_t _physical_mmap_end;
+
+/**
+ * Managed by virtual memory manager
+ */
+extern uint32_t	_page_directory_start;
+extern uint32_t	_page_directory_end;
+extern uint32_t _page_table_entries_start;
+extern uint32_t _page_table_entries_end;
+
+extern uint32_t _DMA_memory_region_start;
+extern uint32_t	_DMA_memory_region_end; // paddr: 0x01000000
 
 #endif // MEMLAYOUT_H_
 
