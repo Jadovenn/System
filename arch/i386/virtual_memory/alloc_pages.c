aca17150e16f44b4a4ee52bbad77c9ec828f0998
Jean-Adrien Domage <jadomage@vente-privee.com>
2020-02-10 00:10:37 +0100

    remove useless memmap that has been moved in memlayout.c

--- arch/i386/virtual_memory/alloc_pages.c
+++ arch/i386/virtual_memory/alloc_pages.c
@@ -10,22 +10,10 @@
 #include <cpu/cr.h>
 #include <arch/paging.h>
 
-/**
- * @brief mark selected pages with the correct values
- */
-static void	*_alloc_pages(arch_vma_t *vma, uint8_t flags) {
-
-}
-
-static arch_vma_t	arch_vma[] = {
-	{ 0x00100000, 0xBFFFFFFF, USER_SPACE },
-	{ 0xC0000000, 0xFFFFFFFF, KERNEL }
-};
-
 void	*allocate_pages(size_t size, memory_zone_t zone) {
 	if (zone < 0 || zone > 1) {
 		return NULL;
 	}
-	return _alloc_pages(&arch_vma[zone], zone);
+	return NULL;
 }
 
