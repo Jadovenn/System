dde56806f2eb1997e4d1d763c16dcbbe9bc99e77
Jean-Adrien Domage <jadomage@vente-privee.com>
2020-02-26 16:31:18 +0100

    big improvement in physical memory init sequence
    The phisical memory init sequence has been re-written and
    now correctedly detect different region and allocate bitmap
    only for region that the kernel will managed. It makes the
    difference between free memory below 16Mib and after.

--- arch/i386/physical_memory/init.c
+++ arch/i386/physical_memory/init.c
@@ -25,71 +25,85 @@
 uint32_t _physical_mmap_start = 0;
 uint32_t _physical_mmap_end = 0;
 
-static void	__display_physical_memory_regions(multiboot_mmap_region_t *mmap) {
-	printk("    addr: %#x, len: %u bytes, type: %d\n", (uint32_t)mmap->addr, (uint32_t)mmap->len, mmap->type);
-}
-
-static inline void	__create_physical_region(uint32_t len, uint32_t addr, uint32_t type) {
-	// Compute region's size in Kib
-	size_t page_nb = len / 1024; // phys map in Kib
-	page_nb /= 4; // nb of phys page 4kib
-	// Compute nb of page available in this region
-	page_nb = page_nb - (page_nb % 32); // align on 32
-	if (!page_nb) {
+static void	__create_physical_region(uint32_t len, uint32_t addr, uint32_t type) {
+	size_t bytes = sizeof(pmm_region_t);
+	size_t usable_page_count = 0;
+	if (type == MEMORY_AVAILABLE || type == MEMORY_LOWER_REGION) {
+		size_t page_count = (len / 1024) / 4;
+		usable_page_count = page_count - (page_count % 32);
+		bytes += usable_page_count / 8;
+	}
+	size_t page_count = (bytes / 0x1000 ? bytes / 0x1000 + 1 : 1);
+	size_t available_bytes = 0x1000 - _physical_mmap_end % 0x1000;
+	if (bytes > available_bytes) {
+		uintptr_t paddr = _physical_mmap_end + (0x1000 - _physical_mmap_end % 0x1000);
+		uintptr_t vaddr = PHYSICAL_ADDR_TO_VIRTUAL(paddr);
+		for (size_t nb_page = 0; nb_page < page_count; nb_page += 1) {
+			if (pg_map(paddr, vaddr, 0x3, false) == EXIT_FAILURE) {
+				PANIC("Fatal error, could not map physical region, more RAM needed");
+			}
+			paddr += 0x1000;
+			vaddr += 0x1000;
+		}
+	}
+	pmm_region_t *new_region = (pmm_region_t*)PHYSICAL_PTR_TO_VIRTUAL((uint32_t*)_physical_mmap_end);
+	_physical_mmap_end += bytes + 1;
+	memset(new_region, 0, bytes);
+	new_region->pstart = addr;
+	new_region->pend = (addr + len - 1); 
+	new_region->bytes = len;
+	new_region->page_nb = usable_page_count;
+	new_region->type = type;
+	new_region->bitset = NULL;
+	new_region->next = NULL;
+	if (type == MEMORY_AVAILABLE || type == MEMORY_LOWER_REGION) {
+		new_region->bitset = (uint32_t*)&new_region[1];
+	}
+	if (!physical_memory_map) {
+		physical_memory_map = new_region;
 		return;
 	}
-	size_t bitset_len = page_nb / 32;
-	size_t size = bitset_len * 4 + sizeof(pmm_region_t);
-	size_t pages = size / 0x1000;
-	if (pages % 0x1000 || !pages) {
-		pages += 1;
-	}
-	pmm_region_t *start = (pmm_region_t*)PHYSICAL_PTR_TO_VIRTUAL((uint32_t*)_physical_mmap_end);
-	uint32_t paddr = _physical_mmap_end;
-	uint32_t vaddr = PHYSICAL_ADDR_TO_VIRTUAL(_physical_mmap_end);
-	for (size_t nb_page = 0; nb_page < pages; nb_page += 1) {
-		if (pg_map(paddr, vaddr, 0x3, false) == EXIT_FAILURE) {
-			PANIC("Could not map safly physical page frame");
-			return;
-		}
-		paddr += 0x1000;
-		vaddr += 0x1000;
-	}
-	_physical_mmap_end = paddr;
-	memset(start, 0, pages * 0x1000);
-	start->physical_addr = addr;
-	start->page_nb = page_nb;
-	start->type = type;
-	if (type == 1 || type == 3) {
-		start->bitset = (uint32_t*)((uint32_t)start + sizeof(pmm_region_t));
-	}
-	else {
-		start->bitset = NULL;
-	}
-	start->next = physical_memory_map;
-	physical_memory_map = start;
+	pmm_region_t *region = physical_memory_map;
+	for (; region->next; region = region->next);
+	region->next = new_region;
 }
 
-static void	__vmap_physical_memory_region_groupe(multiboot_mmap_region_t *mmap) {
+static void	__read_physical_memory_regions_block(multiboot_mmap_region_t *mmap) {
 	if (!(uint32_t)mmap->addr && (uint64_t)mmap->addr) {
 		printk("PAE not supported, physical region ignored\n");
 		return;
 	}
-
-	__create_physical_region(mmap->len, mmap->addr, mmap->type);
+	if (mmap->addr < 0x100000 && mmap->type == MULTIBOOT_MEMORY_AVAILABLE) {
+		__create_physical_region(mmap->len, mmap->addr, MEMORY_BIOS);
+	}
+	else if (mmap->type == MULTIBOOT_MEMORY_AVAILABLE
+			&& mmap->addr + mmap->len - 1 < 0x1000000) {
+		__create_physical_region(mmap->len, mmap->addr, MEMORY_AVAILABLE);
+	}
+	else if (mmap->type == MULTIBOOT_MEMORY_AVAILABLE
+			&& mmap->addr < 0x1000000 && mmap->addr + mmap->len - 1 > 0x1000000) {
+		__create_physical_region(0x1000000 - mmap->addr, mmap->addr, MEMORY_LOWER_REGION);
+		__create_physical_region(mmap->len - (0x1000000 - mmap->addr),
+						mmap->addr + (0x1000000 - mmap->addr), mmap->type);
+	}
+	else {
+		__create_physical_region(mmap->len, mmap->addr, mmap->type);
+	}
 }
 
-static void __display_usable_physical_memory() {
+static __inline void __display_usable_physical_memory() {
 	size_t	size = 0;
+	printk("Physical Memory Regions Map:\n");
 	for (pmm_region_t *idx = physical_memory_map;
 			idx; idx = idx->next) {
-		if (idx->type != 1) {
+		printk("   [%#x : %#x] type: %d, %d Kib of usable memory\n",
+				idx->pstart,
+				idx->pend,
+				idx->type,
+				idx->page_nb*4);
+		if (idx->type != MEMORY_AVAILABLE && idx->type != MEMORY_LOWER_REGION) {
 			continue;
 		}
-		printk("[%#x - %#x] %d Kib of usable memory\n",
-				idx->physical_addr,
-				idx->physical_addr + idx->page_nb * 0x1000,
-				idx->page_nb*4);
 		size += idx->page_nb * 4;
 	}
 	size /= 1024;
@@ -102,30 +116,31 @@ static void __display_usable_physical_memory() {
 	}
 }
 
-static __inline void __map_kernel_physical_region() {
+static __inline void __map_used_physical_region() {
 	uint32_t kernel_start_addr = VIRTUAL_ADDR_TO_PHYSICAL((uint32_t)&_kernel_start);
 	uint32_t kernel_end_addr = VIRTUAL_ADDR_TO_PHYSICAL((uint32_t)&_end);
 	kernel_end_addr -= kernel_end_addr % 0x1000;
-	uint32_t result = pmm_set_region(kernel_start_addr, kernel_end_addr, true);
+	uint32_t result = pmm_set_region(kernel_start_addr, kernel_end_addr, PM_PAGE_PRESENT);
 	if (result != EXIT_SUCCESS) {
 		PANIC("Could not map physical aera of the kernel");
 	}
-	result = pmm_set_region(_physical_mmap_start, _physical_mmap_end - 0x1000, true);
+	result = pmm_set_region(_physical_mmap_start, _physical_mmap_end - 0x1000, PM_PAGE_PRESENT);
 	if (result != EXIT_SUCCESS) {
 		PANIC("Could not map physical aera of the bitset for physical memory manager");
 	}
 }
 
 void	physical_memory_init(multiboot_info *header) {
-	printk("Physical Memory Regions:\n");
-	mltb_foreach_physical_memory_region(header, &__display_physical_memory_regions);
 	_physical_mmap_start = VIRTUAL_ADDR_TO_PHYSICAL((uint32_t)&_end);
 	if (_physical_mmap_start % 0x1000) {
 		_physical_mmap_start += (0x1000 - (_physical_mmap_start % 0x1000));
 	}
 	_physical_mmap_end = _physical_mmap_start;
-	mltb_foreach_physical_memory_region(header, &__vmap_physical_memory_region_groupe);
+	if (pg_map(_physical_mmap_start, PHYSICAL_ADDR_TO_VIRTUAL(_physical_mmap_start), 0x3, false) == EXIT_FAILURE) {
+		PANIC("Fatal error, could not map physical region, more RAM needed");
+	}
+	mltb_foreach_physical_memory_region(header, &__read_physical_memory_regions_block);
 	__display_usable_physical_memory();
-	__map_kernel_physical_region();
+	__map_used_physical_region();
 }
 
