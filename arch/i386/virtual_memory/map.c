ecaa9666fc8d4002507a64e81bb9884f73a4d474
Jean-Adrien Domage <jadomage@vente-privee.com>
2020-02-26 16:38:52 +0100

    update pg_map to handle missing pte
    Now pg_map proc will try to allocate a pte while it is missing.
    If it is two early during the boot or there is no more memory,
    it will return with error if pte is missing.

--- arch/i386/virtual_memory/map.c
+++ arch/i386/virtual_memory/map.c
@@ -8,10 +8,24 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#include <api/mm.h>
 #include <cpu/cr.h>
 #include <cpu/mmu.h>
+#include <arch/physical_memory.h>
 #include <arch/memlayout.h>
 #include <kernel/stdio.h>
+
+void	pg_add_pte(uintptr_t vaddr, uintptr_t paddr) {
+	unsigned offset = vaddr >> 22;
+	uint32_t *pg_dir = (uint32_t*)PHYSICAL_ADDR_TO_VIRTUAL(read_cr3());
+	uint32_t *pte_pages = PHYSICAL_PTR_TO_VIRTUAL((uint32_t*)_page_table_entries);
+	pte_pages[offset] = paddr | 0x3;
+	flush_tlb();
+	uint32_t *pte = pte_pages + offset;
+	memset(pte, 0x1000, 0);
+	pg_dir[offset] = paddr | 0x3;
+	flush_tlb();
+}
 
 uint32_t		pg_map(uint32_t physical_addr, uint32_t virtual_addr, uint32_t flags, bool override) {
 	uint32_t	*pg_dir = (uint32_t*)PHYSICAL_ADDR_TO_VIRTUAL(read_cr3());
@@ -22,8 +36,12 @@ uint32_t		pg_map(uint32_t physical_addr, uint32_t virtual_addr, uint32_t flags, 
 	unsigned pg_dir_offset = virtual_addr >> 22;
 	uint32_t physical_pg_entry_addr = pg_dir[pg_dir_offset];
 	if (!(physical_pg_entry_addr & 0x1)) {
-		printk("ERROR ::: PAGING: Asked page is not present, could not add a new one\n");
-		return EXIT_FAILURE;
+		uintptr_t page = pmm_get_page(MEMORY_AVAILABLE);
+		if (!page) {
+			printk("ERROR ::: PAGING: Asked page is not present, could not add a new one\n");
+			return EXIT_FAILURE;
+		}
+		pg_add_pte(pg_dir_offset * 0x400000, page);
 	}
 	physical_pg_entry_addr &= 0xFFFFF000;
 	uint32_t *pg_table_entry = PHYSICAL_PTR_TO_VIRTUAL((uint32_t*)physical_pg_entry_addr);
@@ -36,17 +54,5 @@ uint32_t		pg_map(uint32_t physical_addr, uint32_t virtual_addr, uint32_t flags, 
 	*pte_ptr = physical_addr | flags;
 	flush_tlb();
 	return EXIT_SUCCESS;
-}
-
-void	pg_add_pte(uintptr_t vaddr, uintptr_t paddr) {
-	unsigned offset = vaddr >> 22;
-	uint32_t *pg_dir = (uint32_t*)PHYSICAL_ADDR_TO_VIRTUAL(read_cr3());
-	uint32_t *pte_pages = PHYSICAL_PTR_TO_VIRTUAL((uint32_t*)_page_table_entries_start);
-	pte_pages[offset] = paddr | 0x3;
-	flush_tlb();
-	uint32_t *pte = pte_pages + offset;
-	memset(pte, 0x1000, 0);
-	pg_dir[offset] = paddr | 0x3;
-	flush_tlb();
 }
 
