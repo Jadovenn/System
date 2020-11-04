10ba0eb33a79f8b82b4206330e9d762c3298cd20
Jean-Adrien Domage <jeanadrien.d@gmail.com>
2020-11-04 14:43:43 +0100

    rename arch/i386/multiboot directory to init_code

--- arch/i386/architecture.mk
+++ arch/i386/architecture.mk
@@ -8,7 +8,7 @@ ARCH_LDFLAGS		=	-Wl,-T$(ARCH_DIR)/i386.ld
 ARCH_INCLUDE_DIRS	=	$(ARCH_DIR)/ \
 						$(ARCH_DIR)/include
 
-ARCH_ASM_SRCS	=		$(ARCH_DIR)/multiboot/multiboot.s \
+ARCH_ASM_SRCS	=		$(ARCH_DIR)/init_code/multiboot.s \
 						$(ARCH_DIR)/cpu/asm/interrupt.s \
 						$(ARCH_DIR)/cpu/asm/dt_flush.s
 
@@ -20,14 +20,15 @@ ARCH_C_SRCS	=	$(ARCH_DIR)/i386_entry.c \
 				$(ARCH_DIR)/cpu/dump_regs.c \
 				$(ARCH_DIR)/cpu/pic/pic.c \
 				$(ARCH_DIR)/cpu/pic/ports.c \
+				$(ARCH_DIR)/memory/memory_init.c \
+				$(ARCH_DIR)/memory/boot/boot_paging.c \
 				$(ARCH_DIR)/memory/memory_layout.c \
-				$(ARCH_DIR)/memory/physical/init.c \
-				$(ARCH_DIR)/memory/physical/alloc.c \
-				$(ARCH_DIR)/memory/virtual/init.c \
+				$(ARCH_DIR)/memory/physical/physical_memory_init.c \
+				$(ARCH_DIR)/memory/physical/physical_memory.c \
+				$(ARCH_DIR)/memory/virtual/virtual_memory_init.c \
 				$(ARCH_DIR)/memory/virtual/heap.c \
-				$(ARCH_DIR)/memory/virtual/map.c \
-				$(ARCH_DIR)/memory/virtual/alloc_pages.c \
-				$(ARCH_DIR)/memory/virtual/pagefault_handler.c \
+				$(ARCH_DIR)/memory/virtual/paging.c \
+				$(ARCH_DIR)/memory/virtual/page_fault_handler.c \
 				$(ARCH_DIR)/drivers/monitor/init.c \
 				$(ARCH_DIR)/drivers/monitor/monitor.c \
 				$(ARCH_DIR)/drivers/timer_example/timer.c
