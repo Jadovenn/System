386638ede0bc320af4e1386a3c8def3f88341e26
Jean-Adrien Domage <jeanadrien.d@gmail.com>
2020-11-05 19:27:04 +0100

    [update] kernel init log

--- kernel/main.c
+++ kernel/main.c
@@ -8,7 +8,7 @@
 int main(int ac, char** av) {
 	(void)ac;
 	(void)av;
-	printk("System - ExoKernel\n");
+	printk("System - ExoKernel version %s\n", VERSION);
 	printk("-----------------------------------------------\n");
 	printk("Main function reached. Arch init passed.\n");
 	printk("System will now halt.\n");
