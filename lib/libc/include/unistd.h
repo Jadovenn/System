e8a612c783b7d4632d406a943403034b61a622e2
Jean-Adrien Domage <jeanadrien.d@gmail.com>
2020-11-06 22:16:23 +0100

    [remove] malloc and free from libc/ unistd.h

--- lib/libc/include/unistd.h
+++ lib/libc/include/unistd.h
@@ -13,9 +13,4 @@
 #include <stddef.h>
 #include <stdint.h>
 
-void libc_init_allocator(uintptr_t vstart, size_t size,
-                         void* (*cb)(size_t*, size_t));
-void* malloc(size_t size);
-void free(void* ptr);
-
 #endif // _UNISTD_H_
