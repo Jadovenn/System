b7b965a8900e6377eac5e6280e8ef87dec0f0bed
Jean-Adrien Domage <jeanadrien.d@gmail.com>
2020-11-18 21:02:06 +0100

    [add] QEMU_ACCEL config argument

--- build.config.cmake
+++ build.config.cmake
@@ -21,9 +21,18 @@ set(KERNEL_NAME System_${KERNEL_VERSION}-${RELEASE_NAME}-${ARCH}_${BOARD})
 
 set(QEMU qemu-system-${ARCH})
 set(QEMU_PHYSICAL_MEM 128M)
+
+if (APPLE)
+    set(QEMU_ACCEL tcg)
+elseif(UNIX AND NOT APPLE)
+    set(QEMU_ACCEL kvm)
+endif()
+
+## CPU unknown, cool to check with absolutely no features
+##set(QEMU_ARGS -accel ${QEMU_ACCEL} -cpu base)
 ## CPU qemu
-##set(QEMU_ARGS -accel tcg -cpu base)
+##set(QEMU_ARGS -accel ${QEMU_ACCEL} -cpu qemu32,+sse)
 ## CPU intel
-set(QEMU_ARGS -accel tcg -cpu coreduo,+sse)
+set(QEMU_ARGS -accel ${QEMU_ACCEL} -cpu coreduo,+sse)
 ## CPU AMD
-##set(QEMU_ARGS -accel tcg -cpu Opteron_G1,+sse)
+##set(QEMU_ARGS -accel ${QEMU_ACCEL} -cpu Opteron_G1,+sse)
